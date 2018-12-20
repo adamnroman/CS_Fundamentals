3003ffe01596d6513548cad741476c5a6d2b1b38
Adam Roman <adamnroman@gmail.com>
2018-12-20 01:22:55 -0500

    completed exercise

---
+++ leap.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin python3
+
+def is_leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            return False
+        return True
+    return False
+
